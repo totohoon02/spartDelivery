9690acc5f4706a322f33c854e659f553bda39257
Klarra <klarra247@gmail.com>
2024-03-15 21:56:07 +0800

    [Feat] 제품 설명란 추가

--- src/main/java/com/sparta/spartdelivery/controller/CartController.java
+++ src/main/java/com/sparta/spartdelivery/controller/CartController.java
@@ -14,10 +14,10 @@ public class CartController {
     @GetMapping("/cart")
     public String viewCart(Model model) {
         List<CartItem> cartItems = Arrays.asList(
-                new CartItem("Classic Cheeseburger", 8.99, 2),
-                new CartItem("Veggie Delight Sandwich", 7.50, 1),
-                new CartItem("Grilled Salmon", 12.99, 3),
-                new CartItem("Pepperoni Pizza", 9.99, 1)
+                new CartItem("Classic Cheeseburger", "yummy", 8.99, 2),
+                new CartItem("Veggie Delight Sandwich","yummy",  7.50, 1),
+                new CartItem("Grilled Salmon", "yummy", 12.99, 3),
+                new CartItem("Pepperoni Pizza", "yummy", 9.99, 1)
         );
         double total = cartItems.stream()
                 .mapToDouble(item -> item.getPrice() * item.getQuantity())
--- src/main/java/com/sparta/spartdelivery/entity/CartItem.java
+++ src/main/java/com/sparta/spartdelivery/entity/CartItem.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CartItem {
     private String menuName;
+    private String description;
     private double price;
     private int quantity;
 }
