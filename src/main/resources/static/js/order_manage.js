38d93a51fd7567edba27494eae7275c1a6b0064a
OKSUchun <oksuchun119@gmail.com>
2024-03-17 15:44:03 +0900

    [feat] 주문 관리 화면 개발

---
+++ src/main/resources/static/js/order_manage.js
@@ -0,0 +1,51 @@
+// 배달 완료 처리
+document.addEventListener('DOMContentLoaded', function() {
+    // 모든 '자세히 보기' 버튼에 이벤트 리스너를 추가합니다.
+    const detailButtons = document.querySelectorAll('.button-delivery');
+    detailButtons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            // 'event.target'을 사용하여 클릭된 버튼에 접근합니다.
+            const orderId = event.target.getAttribute('data-order-id');
+            markOrderAsDelivered(orderId);
+        });
+    });
+});
+
+function markOrderAsDelivered(orderId) {
+    fetch('/orders/' + orderId + '/deliver', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ orderId: orderId })
+    })
+        .then(response => {
+            if(response.ok) {
+                console.log("Order marked as delivered.");
+                window.location.href = "/orders"
+            } else {
+                console.log("Error occurred while updating order status.");
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+// 자세히 보기
+document.addEventListener('DOMContentLoaded', function() {
+    // 모든 '자세히 보기' 버튼에 이벤트 리스너를 추가합니다.
+    const detailButtons = document.querySelectorAll('.button-detail');
+    detailButtons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            // 'event.target'을 사용하여 클릭된 버튼에 접근합니다.
+            const orderId = event.target.getAttribute('data-order-id');
+            goToOrderDetail(orderId);
+        });
+    });
+});
+
+function goToOrderDetail(orderId) {
+    // 주문 ID를 URL에 포함시켜 상세 페이지로 이동
+    window.location.href = '/orders/' + orderId;
+}
+
+
